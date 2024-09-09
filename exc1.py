2fa3c73b81af0f1710781f8353285e0ceb40ac57
idohamrany13882002 <idohamrany13882002@gmail.com>
2024-09-09 07:09:31 +0300

    Add files via upload

---
+++ exc1.py
@@ -0,0 +1,10 @@
+# START
+pizza_slices: int = int(input('how many pizza slices are there? '))
+print(f'each friend gets {pizza_slices // 4} pizza slices')
+# END
+
+# START
+friends: int = int(input('how many friend arrived? '))
+pizza_slices: int = int(input('how many pizza slices are there? '))
+print(f'each friend gets {pizza_slices // friends} pizza slices')
+# END
---
+++ exc4.py
@@ -0,0 +1,21 @@
+# START
+x: int = 10
+while x<=20:
+    print (x)
+    x+=1
+# END
+
+# START
+x: int = 2
+while x <= 20:
+    print(x)
+    x = x + 2
+# END
+
+# START
+x: int = 1
+y: int = int(input('how much to skip?'))
+while x <= 20:
+    print(x)
+    x = x + y
+# END
